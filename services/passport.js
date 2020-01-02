06ca74ee9bd5fa14b076380c1f0cc543d5d88415
Patara <m.c.kmitl@gmail.com>
2020-01-02 13:49:43 +0700

    refactoring async/await

--- services/passport.js
+++ services/passport.js
@@ -22,19 +22,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("accessToken" + accessToken);
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
 
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          console.log("user aleaddy existing");
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = await new User({ googleId: profile.id });
+        done(null, user);
+      }
     }
   )
 );
